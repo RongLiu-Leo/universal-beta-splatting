c2a4cd405d0bcbc7844e89d29beb24c737e4ddc5
Rong Liu <102014841+RongLiu-Leo@users.noreply.github.com>
2025-10-27 12:51:30 -0700

    finish realWorldVideoEvent

--- static/js/event_handler.js
+++ static/js/event_handler.js
@@ -25,6 +25,52 @@ document.addEventListener('DOMContentLoaded', domReady);
             });
         }
 
+        function realWorldVideoEvent(idx) {
+            let sections = document.querySelectorAll('.b-dics.real-world')[0].getElementsByClassName('b-dics__section')
+            for (let i = 0; i < sections.length; i++) {
+                let mediaContainer = sections[i].getElementsByClassName('b-dics__media-container')[0];
+                let media = mediaContainer.getElementsByClassName('b-dics__media')[0];
+
+                let parts = media.src.split('/');
+
+                switch (idx) {
+                    case 0:
+                        parts[parts.length - 2] = 'coffee_martini';
+                        break;
+                    case 1:
+                        parts[parts.length - 2] = 'cook_spinach';
+                        break;
+                    case 2:
+                        parts[parts.length - 2] = 'cut_roasted_beef';
+                        break;
+                    case 3:
+                        parts[parts.length - 2] = 'flame_salmon';
+                        break;
+                    case 4:
+                        parts[parts.length - 2] = 'flame_steak';
+                        break;
+                    case 5:
+                        parts[parts.length - 2] = 'sear_steak';
+                        break;
+                }
+
+                let newSrc = parts.join('/');
+                let newMedia = media.cloneNode(true);
+                newMedia.src = newSrc;
+                mediaContainer.replaceChild(newMedia, media);
+            }
+            let scene_list = document.getElementById("real-world-video").children;
+            for (let i = 0; i < scene_list.length; i++) {
+                if (idx == i) {
+                    scene_list[i].children[0].className = "nav-link active"
+                }
+                else {
+                    scene_list[i].children[0].className = "nav-link"
+                }
+            }
+            dicsDemo.medias = dicsDemo._getMedias();
+        }
+
         function demoVideoEvent(idx) {
             let video = document.querySelectorAll('.b-dics.demo')[0].getElementsByTagName('video')[0];
             let parts = video.children[0].src.split('/');
